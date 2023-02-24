7cfb1ad6bfabe48e06eb5d1917b3fef19a7c6e32
eoghan kenny <eoghank>
2023-02-24 20:04:59 +1300

    add ad app del scripts

---
+++ az-cli/del-ad-apps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Login to your Azure account
+az login
+
+# Set your default subscription
+# az account set --subscription "your-subscription-id"
+
+# Get a list of all app registrations in your Azure AD tenant
+appRegistrations=$(az ad app list --query "[].appId" -o tsv)
+
+# Loop through each app registration and delete it
+for appId in $appRegistrations
+do
+    echo $appId
+    az ad app delete --id $appId
+done
